825570443ebce7be764a788def6dd05ebf823ce7
hyj <hyungcristopher@gmail.com>
2022-12-09 23:30:40 +0800

    LookaLike!
    description

---
+++ Assets/Scripts/LevelUnlock.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUnlock : MonoBehaviour
+{
+
+    [SerializeField] Button[] buttons;
+    int unlockedLevelsNumber;
+
+    private void Start()
+    {
+        if(!PlayerPrefs.HasKey("levelsUnlocked")) {
+            PlayerPrefs.SetInt("levelsUnlocked", 1);
+        }
+
+        unlockedLevelsNumber = PlayerPrefs.GetInt("levelsUnlocked");
+
+        for(int i=0; i < buttons.Length; i++){
+
+            buttons[i].interactable = false;
+        }
+    }
+
+    private void Update()
+    {
+        unlockedLevelsNumber = PlayerPrefs.GetInt("levelsUnlocked");
+
+        for (int i=0; i < unlockedLevelsNumber; i++) {
+            buttons[i].interactable = true;
+        }
+    }
+}
